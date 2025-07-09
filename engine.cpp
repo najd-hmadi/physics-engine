39325ce559b738a10c31d11877b19f854acd29bf
ryku <najdhmadi@gmail.com>
2025-07-09 14:54:07 +0100

    added Euler integration

--- engine.cpp
+++ engine.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#define WIDTH 1200
+#define HEIGHT 900
+const double dt = 0.00001;
+const int substeps = 10000;
 class Entity{
     public:
-    float x,y;
-    float vx =0,vy =0;
-    float ax,ay;
-    float mass;
+    double x,y;
+    double vx =0,vy =0;
+    double ax = 0 ,ay;
+    double mass;
     void step(){
-        vx += ax;
-        vy += ay;
-        x += vx;
-        y += vy;
+        x += vx * dt ;
+        y += vy * dt ;
+        vx += ax * dt ;
+        vy += ay * dt;
     }
 };
 class Circle : public Entity {
     public:
     float radius;
-    Circle(float ay,float x,float y,float radius){
+    Circle(double ay,double x,double y,float radius){
         this->ay = ay;
         this->x = x;
         this->y = y;
@@ -59,10 +63,10 @@ class Solver{
     Circle c1;
     SDL_Event e;
     public:
-    Solver(): c1(0.098f,300,500,30){};
+    Solver(): c1(0.98f,300,500,30){};
     bool init(){
         if(SDL_Init(SDL_INIT_VIDEO) < 0) return false;
-        window = SDL_CreateWindow("physics engine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1200,900,SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("physics engine",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
         if(!window){
             std::cerr <<"no window created";
             return false;
@@ -75,6 +79,11 @@ class Solver{
         is_running = true;
         return true;
     }
+    void out_bounds(Circle &circle){
+        if((circle.y + circle.radius > HEIGHT ) || ( circle.y - circle.radius < 0 ) ) circle.vy *= -0.6 ;
+        if((circle.x + circle.radius > WIDTH ) || ( circle.x - circle.radius < 0 ) ) circle.vx *= -0.6 ;
+
+    }
     void run(){
         while(is_running){
             while(SDL_PollEvent(&e) != 0){
@@ -83,12 +92,15 @@ class Solver{
                 }
             }
 
-            c1.draw_circle(surface,0xffffff);
             
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(10);
             c1.draw_circle(surface,0x000000);
-            c1.step();
+            for(int i = 0; i < substeps; i++){
+                c1.step();
+                out_bounds(c1);
+            }          
+            c1.draw_circle(surface,0xffffff);
+            SDL_UpdateWindowSurface(window);
+            // SDL_Delay(10);
             
         }
     }
